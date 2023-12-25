299510cfe703b0da95cdc5c8efdcccc738f41b69
Myxomop <emtsev.sound@gmail.com>
2023-12-25 15:11:09 +0400

    fix

--- accounts/views.py
+++ accounts/views.py
@@ -95,6 +95,7 @@ class AllUsersView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["user_id"] = self.request.user.id
+        context["profile_user"] = self.request.user
         return context
 
 
