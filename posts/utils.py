3be38c891c893e3bcaecb13f0fa3f166b534eca2
Myxomop <emtsev.sound@gmail.com>
2024-01-04 16:01:15 +0400

    remade hashtag_handler

--- posts/utils.py
+++ posts/utils.py
@@ -7,15 +7,29 @@ from posts.models import Hashtag
 
 M = t.TypeVar("M", bound=Model)
 
+"""
+Вирішив залишити другий варіант, так як хештегів не має бути дуже багато (хоча
+якщо користувачів мільйони то вже питання), а функція виходить набагато простіша
+і зрозуміліша. В першому варіанті зробив ignore_conflicts=True, тому що False 
+йде по дефолту і викликає IntegrityError якщо раптом станеться помилка і функція
+таки спробує додати хештег повторно. Я розумію, що достатньо просто залишити 
+існуючий екземпляр, тому додав gnore_conflicts=True  
+"""
+
+# def hashtag_handler(post: M, hashtags: list[str]) -> None:
+#     existing_hashtags = Hashtag.objects.filter(name__in=hashtags)
+#     _new_names = set(hashtags) - set(
+#         hashtag.name for hashtag in existing_hashtags
+#     )
+#     if _new_names:
+#         new_hashtags = [Hashtag(name=name) for name in _new_names]
+#         Hashtag.objects.bulk_create(new_hashtags, ignore_conflicts=True)
+#         post.hashtags.add(*new_hashtags)
+#     post.hashtags.add(*existing_hashtags)
+
 
 def hashtag_handler(post: M, hashtags: list[str]) -> None:
-    existing_hashtags = Hashtag.objects.filter(name__in=hashtags)
-    existing_hashtag_names = (hashtag.name for hashtag in existing_hashtags)
-    new_hashtags = [
-        Hashtag(name=name)
-        for name in hashtags
-        if name not in existing_hashtag_names
+    _hashtags = [
+        Hashtag.objects.get_or_create(name=name)[0] for name in hashtags
     ]
-    Hashtag.objects.bulk_create(new_hashtags)
-    for hashtag in chain(existing_hashtags, new_hashtags):
-        post.hashtags.add(hashtag)
+    post.hashtags.add(*_hashtags)
