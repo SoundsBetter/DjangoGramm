e76adf141f81c4a297d41853a2977a157eb1ee8c
Myxomop <emtsev.sound@gmail.com>
2024-01-28 16:22:47 +0400

    temp

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,11 @@
+from functools import partial
+
 from cloudinary.models import CloudinaryField
 from django.db import models
 from django.db.models import UniqueConstraint
 
-
+from DjangoGramm.settings import PICTURES
+from DjangoGramm.utils import directory_path
 from auths.models import User
 
 
@@ -23,7 +26,10 @@ class Photo(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="photos"
     )
-    picture = CloudinaryField("picture")
+    picture = models.ImageField(
+        upload_to=partial(directory_path, base_folder=PICTURES)
+    )
+    # picture = CloudinaryField("picture")
 
     def delete(self, *args, **kwargs):
         file = self.picture
