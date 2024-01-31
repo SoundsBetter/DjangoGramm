95d52744b68a031685a7849d5834d528289e1c43
Myxomop <emtsev.sound@gmail.com>
2024-01-31 14:43:57 +0400

    for migrate to cloudinary

---
+++ accounts/management/commands/migrateavatars.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+from accounts.models import UserProfile
+import cloudinary.uploader
+
+
+class Command(BaseCommand):
+    help = "Migrate avatars to Cloudinary"
+
+    def handle(self, *args, **kwargs):
+        for user in UserProfile.objects.all():
+            response = cloudinary.uploader.upload(user.avatar.path)
+            user.avatar = response["url"]
+            user.save()
+
+            self.stdout.write(
+                self.style.SUCCESS(f"Successfully migrated avatar {user.pk}")
+            )
--- accounts/models.py
+++ accounts/models.py
@@ -9,10 +9,15 @@ from auths.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = CloudinaryField("avatar", null=True)
-    date_of_birth = models.DateField(
-        blank=True, null=True, verbose_name="Date of birth"
+    avatar = models.ImageField(
+        upload_to=partial(directory_path, base_folder=AVATARS),
+        blank=True,
+        null=True,
     )
+    # avatar = CloudinaryField("avatar", null=True)
+    # date_of_birth = models.DateField(
+    #     blank=True, null=True, verbose_name="Date of birth"
+    # )
     phone_number = models.CharField(
         max_length=17,
         blank=True,
